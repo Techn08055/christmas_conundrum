d4bb1ee83709ab17df009c86602274d6b42f43ea
Techn08055 <technomadlyf@gmail.com>
2022-12-12 01:20:16 +0000

    first commit and added raw data

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='To find the optimum path to print an image',
+    author='chris_8055',
+    license='MIT',
+)
